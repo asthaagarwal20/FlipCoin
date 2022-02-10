d157d6cc8f548c80594eb28e2f757fe518827dd0
unknown <asthaagarwal203@gmail.com>
2022-02-10 16:14:10 +0530

    [Astha] Add:FlipCoinUC1 solved

---
+++ FlipCoin/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            if (random.Next(0, 2) == 0)
+                Console.WriteLine("Head");
+            else
+                Console.WriteLine("Tail");
+        }
+    }
+}
